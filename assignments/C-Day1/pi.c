2d198e82fdaf95e2d51b4d31bb5d89caf9a0f221
Juan Miguel Valois Martinez <jmvalois@login2.frontera.tacc.utexas.edu>
2023-06-06 11:07:00 -0500

    PI CODE

--- assignments/C-Day1/pi.c
+++ assignments/C-Day1/pi.c
@@ -4,8 +4,25 @@
 
 int main(int argc, char **argv) {
 
+    if (argc != 2) {
+    printf("Usage: appName numSteps\n");
+    exit(-1);
+  }
+
+  double numSteps = atof(argv[1]);
   double pi   = 0;
-  
+  /*  printf("%.4f\n",numSteps);*/
+  double delta_x = 1/numSteps;
+  double sum = 0;
+  for(double i=0;i<numSteps;i++){
+    
+    double x = i*delta_x+delta_x/2;
+    double fun = 4.0/(1+x*x);
+    sum = sum + fun*delta_x;
+    /* printf("%.4f\n",x);*/
+  }
+  pi = sum;
+
   printf("PI = %16.14f, diff(%16.14f)\n",pi, fabs(pi-M_PI));
   return 0;
 }
